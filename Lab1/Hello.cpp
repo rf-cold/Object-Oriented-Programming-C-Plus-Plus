06f9791877ee432ab1b2aa6bfeb82a92e5562da7
rf-cold <foxtrot7111@gmail.com>
2025-02-13 15:42:56 -0800

    Folder

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,13 @@
+/***
+ * Introductory Lab
+ */
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    cout <<"Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+}
